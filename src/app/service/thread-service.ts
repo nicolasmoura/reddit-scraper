978386164720c703a5e2346f7a2b9eea98d181d7
nicolasmoura <nicolas-md@hotmail.com>
2018-01-15 23:52:02 -0200

    adicionando variável de ambiente com endpoint da API no heroku

--- src/app/service/thread-service.ts
+++ src/app/service/thread-service.ts
@@ -2,14 +2,14 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Thread} from "../model/thread";
 import {Observable} from "rxjs/Observable";
-
+import {environment} from '../../environments/environment'
 @Injectable()
 export class ThreadService {
 
     constructor(private http: HttpClient) {}
 
     getHotThreads(subreddits: string): Observable<Array<Thread>> {
-        return this.http.get<Array<Thread>>('http://localhost:1346/threads/hot', {
+        return this.http.get<Array<Thread>>(environment.API_ENDPOINT + '/threads/hot', {
             params: new HttpParams().set('subreddits', subreddits)
         });
     }
